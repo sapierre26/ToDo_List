8115a259cc02d68a976fa8aa5714206c51ae70b2
Kailuan Liu <kailuan1127@gmail.com>
2025-02-27 10:09:58 -0800

    frontend folder change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,50 +1,17 @@
 //ToDoList
 import React from 'react'
 import './App.css'
-<<<<<<< HEAD
-import Month from "./Month"
-import Week from "./Week"
-import Day from "./Day"
-import SplitScreen from "./SplitScreen.jsx"
-=======
+
+// import SplitScreen from "./SplitScreen.jsx"
 import CalendarComponent from './CalendarComponent';
->>>>>>> main
+
 
 function App() {
 	return (
-<<<<<<< HEAD
-		<div className="buttons">
-			<button onClick={() => setView('month')}>Month</button> <button onClick={() => setView('week')}>Week</button>  <button onClick={() => setView('day')}>Day</button>
-			<div className="container">
-			{view == 'month' ? (
-					<Month 
-						month={month} 
-						year={year}
-					/> 
-				) : view === 'week' ? (
-					<Week 
-						day={day}
-						month={month}
-						year={year} 
-					/>
-				) : (
-					<Day
-						day={day}
-						month={month}
-						year={year} 
-					/>
-				)
-			}
-			</div>
-		</div>
-	);
-
-=======
         <div style={{ height: "95vh", width: "100%", padding: "20px", display: "block", justifyContent: "center", alignItems: "center" }}>
             <CalendarComponent />
         </div>
     );
->>>>>>> main
 }
 
 export default App;
