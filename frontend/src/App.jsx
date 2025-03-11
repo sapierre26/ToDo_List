cab7554e492d3566a20f34e832352332854434d7
Kailuan Liu <kailuan1127@gmail.com>
2025-03-11 12:02:14 -0700

    createaccount page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,21 +4,24 @@ import './App.css';
 import CalendarComponent from './components/Calendar/page';
 import TodoList from './components/todolist/page';
 import Login from './components/Login/page';
+import CreateAccount from './components/CreateAccount/page';
 
 function App() {
     return (
         <Router>
             <div style={{ height: "95vh", width: "100%", padding: "20px" }}>
                 <nav style={{ marginBottom: '20px' }}>
-                    <Link to="/calendar" className="button-link">Calendar</Link>
-                    <Link to="/todolist" className="button-link">Todo List</Link>
-                    <Link to="/login" className="button-link">Login</Link>
+                    <Link to="/Calendar" className="button-link">Calendar</Link>
+                    <Link to="/Todolist" className="button-link">Todo List</Link>
+                    <Link to="/Login" className="button-link">Login</Link>
+                    <Link to="/createAccount" className="button-link">Create Account</Link>
                 </nav>
 
                 <Routes>
-                    <Route path="/calendar" element={<CalendarComponent />} />
-                    <Route path="/todolist" element={<TodoList />} />
-                    <Route path="/login" element={<Login />} />
+                    <Route path="/Calendar" element={<CalendarComponent />} />
+                    <Route path="/Todolist" element={<TodoList />} />
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/createAccount" element={<CreateAccount/>} />
                 </Routes>
             </div>
         </Router>
