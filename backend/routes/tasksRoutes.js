def7a84868f8caae9e03a8ff358b201735d39f83
Kailuan Liu <kailuan1127@gmail.com>
2025-03-13 00:16:24 -0700

    calendar page

--- backend/routes/tasksRoutes.js
+++ backend/routes/tasksRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const Task = require("../models/taskSchema.js");
+// app.use(express.json());  
 
+const Task = require("../models/taskSchema.js");
 router.use("/", (req, res, next) => {
   console.log(`Request made to ${req.method} ${req.originalUrl}`);
   next(); // pass control to the next handler
@@ -85,4 +86,50 @@ router.put("/", async (req, res) => {
   }
 });
 
-module.exports = router;
+router.delete("/:_id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const task = await Task.findByIdAndDelete(id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    return res.status(200).json({ message: "Task deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting task:", error);
+    return res.status(500).json({ message: "Error deleting task" });
+  }
+});
+
+// router.get("/date/:date", async (req, res) => {
+//   try {
+//     const task = await Task.findOne({ date: req.params.date })
+//     console.log("reached here")
+//     res.send(task)
+//     console.log('got task with id %s', req.params.date)
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// })
+
+router.get("/", async (req, res) => {
+  try {
+    const { date } = req.query; // Extract 'date' from the query parameters
+
+    let tasks;
+    if (date) {
+      // If the 'date' query parameter is provided, filter tasks by date
+      tasks = await Task.find({ date: date });
+    } else {
+      // If no 'date' parameter is provided, return all tasks
+      tasks = await Task.find({});
+    }
+
+    console.log("Got tasks:", tasks);
+    res.send(tasks);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
