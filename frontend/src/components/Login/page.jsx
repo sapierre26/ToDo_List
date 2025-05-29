7be2150d49f29727d7ce72e433777453be116b5f
Sanaia <sgpierre2003@gmail.com>
2025-05-29 03:43:19 -0700

    When you successfully login go straight to the split screen

--- frontend/src/components/Login/page.jsx
+++ frontend/src/components/Login/page.jsx
@@ -55,7 +55,7 @@ const Login = ({ onLoginSuccess }) => {
       }
 
       if (onLoginSuccess) onLoginSuccess();
-      navigate("/Calendar");
+      navigate("/SplitScreen");
     } catch (err) {
       setErrorMessage(err.message);
       console.error("Error during login:", err.message);
