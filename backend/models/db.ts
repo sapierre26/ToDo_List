d29380afa0a3af0f26826cf340c11ab12345408b
Sanaia <sgpierre2003@gmail.com>
2025-05-29 02:02:15 -0700

    Changes made to db.ts

--- backend/models/db.ts
+++ backend/models/db.ts
@@ -3,7 +3,8 @@ dotenv.config();
 
 import mongoose from "mongoose";
 
-const url: string = process.env.MONGO_URI as string;
+const url: string | undefined = process.env.MONGO_URI;
+// = process.env.MONGO_URI as string;
 let connection: typeof mongoose;
 
 /**
@@ -11,11 +12,25 @@ let connection: typeof mongoose;
  * Call this function at the start of api routes and data fetches
  * @returns {Promise<typeof mongoose>}
  */
-const connectDB = async () => {
+// const connectDB = async () => {
+//   if (!connection) {
+//     connection = await mongoose.connect(url);
+//     return connection;
+//   }
+// };
+
+const connectDB = async (): Promise<typeof mongoose | undefined> => {
+  if (!url) {
+    console.error("❌ MONGO_URI is not set in environment variables.");
+    process.exit(1);
+  }
+
   if (!connection) {
     connection = await mongoose.connect(url);
-    return connection;
+    console.log("✅ MongoDB connected.");
   }
+
+  return connection;
 };
 
 export default connectDB;
